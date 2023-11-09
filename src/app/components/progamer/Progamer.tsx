398b633c15f347ec7383cc43139d19dd866a3b92
reiym1 <subaru4791@gmail.com>
2023-11-09 20:43:14 +0900

    change

--- src/app/components/progamer/Progamer.tsx
+++ src/app/components/progamer/Progamer.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React,{ FC } from "react";
+import Link from "next/link";
 
+import styles from "./styles/progamer.module.scss";
+
+type progamerProps = {
+  imgPath: string;
+  progamerName: string;
+  progamerId: string;
+};
 const Progamer = () => {
   return <div>Progamer</div>;
+
 };
 
 export default Progamer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,32 @@ export default function Home() {
       <h1>推し活をはかどらせよう!</h1>
       <div>
         <div>
+          <h1>LOGIN</h1>
+        </div>
+        <div>
           <main>
           <title>推し活をはかどらせよう!</title>
           </main>
+          <div>
+          <h2>PROGAMER</h2>
+          <h3>STREAMER</h3>
+          </div>
+          <h4>PARTSLIST</h4>
+          <ul>
+            <li>モニター</li>
+            <li>キーボード</li>
+            <li>マウス</li>
+            <li>マウスパッド</li>
+            <li></li>
+          </ul>
+          <div>
+            <h5>検索バー</h5>
+          </div>
+          <div>
+            <h6>REVIEW</h6>
+          </div>
           {/*<LOGIN />
-          <MYPAGE />
+          <MYPAGE(もしかしたらログインコンポーネントにマイページコンポーネントを入れるかも？) />
           <STREAMER />
           <PROGAMER />
           <REVIEW />  */}
